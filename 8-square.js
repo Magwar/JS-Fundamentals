cc86cd49d7a746f098dc29202ca5516d9ef39eee
magwar <williammagua@gmail.com>
2025-05-24 09:39:26 +0300

    Square printing as per argument

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+let square = parseInt(process.argv[2]);
+
+isNaN(square) && console.log("Missing size");
+
+!isNaN(square) && (function() {
+    for (let i = 0; i < square; i++) {
+        console.log("x".repeat(square));
+    }
+})();
